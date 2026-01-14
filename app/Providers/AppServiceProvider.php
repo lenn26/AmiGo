8ebdb7038b8003d9c833e7f6145fb39ae53b8a0a
Lenny <lennycharles2610@gmail.com>
2026-01-14 02:38:52 +0100

    fix: AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,9 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
         // Format des dates en français
         \Carbon\Carbon::setLocale('fr');
 
